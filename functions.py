6bf0374b80c13f25916e3626a740412df68cf117
Victor Perez <vparmenta@uma.es>
2023-11-03 09:50:22 +0100

    prueba pasaporte 2
    Pytesseract no reconoce bien esta fuente

--- functions.py
+++ functions.py
@@ -1,26 +1,59 @@
 import cv2
 import numpy as np
 import pytesseract as pts
+import matplotlib.pyplot as plt
 
-#pts.pytesseract.tesseract_cmd = r'C:\Users\vparm\AppData\Local\Programs\Tesseract-OCR\tesseract.exe'
-pts.pytesseract.tesseract_cmd = r'C:\Users\Victor\AppData\Local\Programs\Tesseract-OCR\tesseract.exe'
+
+pts.pytesseract.tesseract_cmd = r'C:\Users\vparm\AppData\Local\Programs\Tesseract-OCR\tesseract.exe'
+
+
+# pts.pytesseract.tesseract_cmd = r'C:\Users\Victor\AppData\Local\Programs\Tesseract-OCR\tesseract.exe'
+
+def auto_subplotter(size_x, size_y, d):
+    i = 1
+    for pair in d.items():
+        plt.subplot(size_x, size_y, i)
+        plt.title(pair[1])
+        plt.imshow(pair[0])
+        i += 1
+
+    plt.show()
+
 
 def load_image(path):
     return cv2.imread(path)
+
 
 def show_image(w_name, image):
     cv2.imshow(w_name, image)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-def subImage(image, x,y,w,h):
-    return image[y:y+h,x:x+w]
-def extract_text_from_image(image):
-    gray_image = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
 
-    threshold_image = cv2.threshold(gray_image,0,255,cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+def subImage(image, x, y, w, h):
+    return image[y:y + h, x:x + w]
+
+
+def extract_text_from_image(image):
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    threshold_image = cv2.threshold(gray_image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
 
     text = pts.image_to_string(threshold_image)
     return text
 
 
+def get_name(image):
+    return subImage(image, 26, 439, 344, 56)
+
+
+def get_sex(image):
+    return subImage(image, 339, 548, 186, 34)
+
+
+def get_issuer(image):
+    return subImage(image, 339, 582, 251, 36)
+
+
+def get_expedition(image):
+    return subImage(image, 339, 617, 251, 40)
--- main.py
+++ main.py
@@ -1,12 +1,39 @@
 import functions as f
+import matplotlib
+matplotlib.use("TkAgg")
+import matplotlib.pyplot as plt
+
 if __name__ == '__main__':
-    path = "images/pasaporte prueba.png"
+    path = "images/pasaporte prueba2.png"
     image = f.load_image(path)
-    print(len(image))
-    f.show_image("passport", image)
-    image_cropped = f.subImage(image,26,439,344,56)
-    text = f.extract_text_from_image(image_cropped)
-    print(text)
-    f.show_image("cropped", image_cropped)
+    name = f.get_name(image)
+    sex = f.get_sex(image)
+    iss = f.get_issuer(image)
+    exp = f.get_expedition(image)
 
+    plt.subplot(221)
+    plt.title("Name")
+    plt.imshow(name)
 
+    plt.subplot(222)
+    plt.title("Sex")
+    plt.imshow(sex)
+
+    plt.subplot(223)
+    plt.title("Iss")
+    plt.imshow(iss)
+
+    plt.subplot(224)
+    plt.title("Exp")
+    plt.imshow(exp)
+    # f.show_image("passport", image)
+    # image_cropped = f.subImage(image,26,439,344,56)
+    text1 = f.extract_text_from_image(name)
+    print(text1)
+    text1 = f.extract_text_from_image(sex)
+    print(text1)
+    text1 = f.extract_text_from_image(iss)
+    print(text1)
+    text1 = f.extract_text_from_image(exp)
+    print(text1)
+    # f.show_image("cropped", image_cropped)
